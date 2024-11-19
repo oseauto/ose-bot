893bfd5c1f1b8eba760c452c185ce7948bedeb27
oseauto <razithrazith71@gmail.com>
2024-11-19 15:49:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,7 +23,7 @@ API_HASH = environ.get('API_HASH', '2846ce47f16b6044cfb5cf245c0d2767')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
-CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
+CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/b69af2db776e4e85d21ec.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/How_To_Open_Linkl")
