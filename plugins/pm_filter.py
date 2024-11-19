26244f882eb79955a4968dd13b258b1f6acda4fc
oseauto <razithrazith71@gmail.com>
2024-11-19 17:08:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2911,9 +2911,9 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
                 cap += f"<b>\n📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file['file_id']}'>[{get_size(file['file_size'])}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file['file_name'].split()))}\n</a></b>"
     else:
         if settings["button"]:
-            cap = f"<b>Rᴇsᴜʟᴛs ғᴏʀ 🔍 {search}</b>"
+            cap = f"<b>Rᴇsᴜʟᴛs ғᴏʀ 🔍 `{search}`</b>"
         else:
-            cap = f"<b>Rᴇsᴜʟᴛs ғᴏʀ 🔍 {search}</b>"
+            cap = f"<b>Rᴇsᴜʟᴛs ғᴏʀ 🔍 `{search}`</b>"
             cap+="<b><u>🍿 Your Movie Files 👇</u></b>\n\n"
             for file in files:
                 cap += f"<b>📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file['file_id']}'>[{get_size(file['file_size'])}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file['file_name'].split()))}\n\n</a></b>"
